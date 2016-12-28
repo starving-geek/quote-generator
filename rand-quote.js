d3962c6e9738b6367c679c931a59a598aea38651
Tyler <tyler@geekingdoing.com>
2016-12-28 17:15:17 -0500

    updated script

---
+++ rand-quote.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // displays a quote generated randomly
+});
+
+function randomQuote(argument) {
+    var quotes = [
+    "Get in loser. We're going shopping.",
+    "Ex-boyfriends are off-limits to friends. That’s just, like, the rules of feminism.",
+    "She doesn’t even go here!",
+    "Stop trying to make fetch happen. It's not going to happen."
+    ];
+}
