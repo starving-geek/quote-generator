29bded9b2a7824b83dfcb3cdced40a7c023b5a28
starving-geek <cleancoder01@gmail.com>
2017-01-02 12:42:16 -0500

    finished random quote function

--- rand-quote.js
+++ rand-quote.js
@@ -2,11 +2,18 @@ $(document).ready(function() {
     // displays a quote generated randomly
 });
 
-function randomQuote(argument) {
+function randomQuote() {
     var quotes = [
-    "Get in loser. We're going shopping.",
-    "Ex-boyfriends are off-limits to friends. That’s just, like, the rules of feminism.",
-    "She doesn’t even go here!",
-    "Stop trying to make fetch happen. It's not going to happen."
+        "Get in loser. We're going shopping.",
+        "Ex-boyfriends are off-limits to friends. That’s just, like, the rules of feminism.",
+        "She doesn’t even go here!",
+        "Stop trying to make fetch happen. It's not going to happen."
     ];
+
+    var randIndex = getRandomInt(0, quotes.length); // numbers from 0 to the length of the quote length
+    return quotes[randIndex];
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
